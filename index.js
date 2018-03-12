712030e1b1661ee5752cdb77a1f3eca8b302bf52
ShubhamSangwan <36914435+ShubhamSangwan@users.noreply.github.com>
2018-03-12 17:54:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+ handle["/"] = requestHandlers.index;
+ handle["/index.html"] = requestHandlers.index;
+ handle["/whatwedo.html"] = requestHandlers.whatwedo;
+ handle["/news.html"] = requestHandlers.news;
+ handle["/whoweare.html"] = requestHandlers.whoweare;
+ handle["/blogs.html"] = requestHandlers.blog;
+ handle["/getinvolved.html"] = requestHandlers.getinvolved;
+ handle["/wherewework.html"] = requestHandlers.wherewework;
+ handle["/cookiepolicy.html"] = requestHandlers.cookiepolicy;
+ handle["/donate.html"] = requestHandlers.donate;
+ handle["/donateform.html"] = requestHandlers.donateform;
+ handle['/contactus.html'] = requestHandlers.contactus;
+ handle["/donateportal.html"] = requestHandlers.donateportal;
+
+ handle["/login.html"] = requestHandlers.login;
+ handle["/forgetpassword.html"] = requestHandlers.forgetpassword;
+ handle["/dashboard.html"] = requestHandlers.dashboard;
+ handle["/blogform.html"] = requestHandlers.blogform;
+ handle["/newsform.html"] = requestHandlers.newsform;
+ handle["/annualreport.html"] = requestHandlers.annualreport;
+ 
+ handle["/css/style.css"] = requestHandlers.style;
+
+ handle["/authenticate.json"] = requestHandlers.authenticatefunction;
+ handle["/blogentry.json"] = requestHandlers.functionblogentry;
+ handle["/whoweare.json"] = requestHandlers.functionwhoweare;
+
+ handle["/js/login.js"] = requestHandlers.scriptLogin;
+ handle["/js/blogform.js"] = requestHandlers.scriptblogform
+ handle["/js/newsform.js"] = requestHandlers.scriptnewsform;
+ handle["/js/whoweare.js"] = requestHandlers.scriptwhoweare;
+ handle["/js/cookie.js"] = requestHandlers.scriptcookie;
+
+ handle["/img/main.jpg"] = requestHandlers.headerImage;
+ handle["/img/about.jpg"] = requestHandlers.aboutImage;
+ handle["/img/whoweare.jpg"] = requestHandlers.whoWeAreHeaderImage;
+ handle["/img/getinvolved.jpg"] = requestHandlers.getInvolvedHeaderImage;
+ handle["/img/wherewework.jpg"] = requestHandlers.whereWeWorkHeaderImage;
+ handle["/img/donate.jpg"] = requestHandlers.donateImage;
+
+ handle["/img/linkimage/fullsize/whoweare.png"] = requestHandlers.whoWeAreImage;
+ handle["/img/linkimage/fullsize/whatwedo.png"] = requestHandlers.whatwedoImage;
+ handle["/img/linkimage/fullsize/contribute.png"] = requestHandlers.getInvolvedImage;
+
+ handle["/img/defaultprofile.png"] = requestHandlers.defaultprofile;
+
+ handle["/img/news/defaultpic.png"] = requestHandlers.defaultpric;
+ // handle["/img/news/foxnews2.jpg"] = requestHandlers.newsImage2;
+ // handle["/img/news/foxnews3.jpg"] = requestHandlers.newsImage3;
+
+ handle["/img/socialmediaicon/facebook.png"] = requestHandlers.facebook;
+ handle["/img/socialmediaicon/googleplus.png"] = requestHandlers.googleplus;
+ handle["/img/socialmediaicon/instagram.png"] = requestHandlers.instagram;
+ handle["/img/socialmediaicon/snapchat.png"] = requestHandlers.snapchat;
+ handle["/img/socialmediaicon/twitter.png"] = requestHandlers.twitter;
+
+ handle["/img/missionVision.jpg"] = requestHandlers.missionVisionImage;
+ handle["/img/shareWarmth.jpg"] = requestHandlers.shareWarmthImage;
+ 
+server.index(router.route, handle);
---
+++ requesthandlers.js
@@ -0,0 +1,434 @@
+var database = require("C:/Users/Shubham/Desktop/WinterHUg/database");
+var saveAs = require('file-saver');
+var fs = require("fs");
+var OK = 200;
+
+var querystring = require("querystring");
+
+// ******************************************************************************* html *************************************************************************
+function index(response, postData, pathname, type) {
+ 	console.log("index request is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+function whatwedo(response, postData, pathname, type) {
+ 	 console.log("whatwedo request is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+ function blog(response, postData, pathname, type) {
+ 	 console.log("blog request is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+ function defaultprofile(response, postData, pathname, type) {
+ 	 console.log("default profile picture request is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+ 
+ function whoweare(response, postData, pathname, type) {
+ 	 console.log("who we are html is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+ function news(response, postData, pathname, type) {
+ 	 console.log("news html is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+ function getinvolved(response, postData, pathname, type) {
+ 	 console.log("get involved html page is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+ function wherewework(response, postData, pathname, type) {
+ 	 console.log("where we work html page is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+ function cookiepolicy(response, postData, pathname, type) {
+ 	console.log("cookiepolicy html page is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+  function donate(response, postData, pathname, type) {
+ 	console.log("donate html page is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+ function donateform(response, postData, pathname, type) {
+ 	console.log("donate-form html page is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+ 
+ function donateportal(response, postData, pathname, type) {
+ 	console.log("donateportal html page is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+ function annualreport(response, postData, pathname, type) {
+ 	console.log("donateportal html page is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+ function contactus(response, postData, pathname, type) {
+ 	console.log(" contactus html page is handled");
+	 var file = "./public" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+  function login(response, postData, pathname, type) {
+ 	 console.log("login html page is handled");
+	 var file = "./public/admin" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+ function forgetpassword(response, postData, pathname, type) {
+ 	 console.log("forgot password html page is handled");
+	 var file = "./public/admin" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+ function dashboard(response, postData, pathname, type){
+	console.log("dashboard html page is handled");
+	 var file = "./public/admin" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+}
+
+function blogform(response, postData, pathname, type) {
+ 	 console.log("blog form html page is handled");
+	 var file = "./public/admin" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+ function newsform(response, postData, pathname, type) {
+ 	 console.log("news form html page is handled");
+	 var file = "./public/admin" + pathname;
+     fs.readFile(file, ready);
+     function ready(err, content) { deliver(response, type, err, content); }
+ }
+
+// ************************************************************************************* json *********************************************************************
+
+function functionwhoweare(response, postData, pathname, type) {
+ 	console.log("dashboard html request is handled");
+	database.blogdataretrieval(function(result){
+			// console.log(result.tostring());
+			type = "application/json";
+			var err;
+			deliver(response, type, err, result.tostring());
+	});
+ }
+function authenticatefunction(response, postData, pathname, type) {
+ 	 console.log("dashboard html request is handled");
+
+	var formData = querystring.parse(postData);
+	database.authentication(formData.username, formData.password, function(result){
+		// console.log(result);
+		if(result == 1){
+			content = '{data:'+result.toString() +'}';
+			var json = JSON.stringify(eval("(" + content + ")"));
+			type = "application/json";
+			var err;
+			deliver(response, type, err, json);
+		}else{
+			content = '{data:'+result.toString() +'}';
+			var json = JSON.stringify(eval("(" + content + ")"));
+			type = "application/json";
+			var err;
+			deliver(response, type, err, json);
+		}
+	});
+ }
+
+ function functionblogentry(response, postData, pathname, type) {
+ 	 console.log("dashboard html request is handled");
+
+	var formData = querystring.parse(postData);
+	database.blogEntry(formData.topic, formData.text, formData.imageName, function(result){
+		if(result == 0){
+			// var arrayBuffer = formData.image;
+   //  		var bytes = new Uint8Array(arrayBuffer);
+
+   //  		saveAs(bytes, formData.imageName+".jpg");
+		}
+		if(result == 0){
+			content = '{data:'+result.toString() +'}';
+			var json = JSON.stringify(eval("(" + content + ")"));
+			type = "application/json";
+			var err;
+			deliver(response, type, err, json);
+		}
+		// }else{
+		// 	content = '{data:'+result.toString() +'}';
+		// 	var json = JSON.stringify(eval("(" + content + ")"));
+		// 	type = "application/json";
+		// 	var err;
+		// 	deliver(response, type, err, json);
+		// }
+	});
+ }
+ 
+// ************************************************************************************* css *********************************************************************
+
+function style(response, postData, pathname, type){
+	console.log("style.css request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+
+// ***************************************************************************************javascript******************************************************************
+
+function scriptLogin(response, postData, pathname, type){
+	console.log("login script request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+
+function scriptblogform(response, postData, pathname, type){
+	console.log("login script request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+
+function scriptnewsform(response, postData, pathname, type){
+	console.log("scriptnewsform script request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+
+function scriptwhoweare(response, postData, pathname, type){
+	console.log("scriptwhoweare script request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+function scriptcookie(response, postData, pathname, type){
+	console.log("scriptcookie script request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+
+// *****************************************************************************images*****************************************************************************
+
+function headerImage(response, postData, pathname, type){
+	console.log("main page headerImage request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+function whoWeAreHeaderImage(response, postData, pathname, type){
+	console.log("who we are headerImage request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+function getInvolvedHeaderImage(response, postData, pathname, type){
+	console.log("get involved headerImage request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+function whereWeWorkHeaderImage(response, postData, pathname, type){
+	console.log("where we work headerImage request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+function whoWeAreImage(response, postData, pathname, type){
+	console.log("whoWeAre Image request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+
+function whatwedoImage(response, postData, pathname, type){
+	console.log("whatwedo Image request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+
+function donateImage(response, postData, pathname, type){
+	console.log("donate Image request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+
+function getInvolvedImage(response, postData, pathname, type){
+	console.log("Get Involved Image request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+
+function defaultpric(response, postData, pathname, type){
+	console.log("newsImage1 Image request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+// function newsImage2(response, postData, pathname, type){
+// 	console.log("newsImage2 Image request is handled");	
+// 	var file = "./public" + pathname;
+// 	fs.readFile(file, ready);
+// 	function ready(err, content) { deliver(response, type, err, content);}
+// }
+// function newsImage3(response, postData, pathname, type){
+// 	console.log("newsImage3 Image request is handled");	
+// 	var file = "./public" + pathname;
+// 	fs.readFile(file, ready);
+// 	function ready(err, content) { deliver(response, type, err, content);}
+// }
+function facebook(response, postData, pathname, type){
+	console.log("facebook Image request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+function googleplus(response, postData, pathname, type){
+	console.log("googleplus Image request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+function instagram(response, postData, pathname, type){
+	console.log("instagram Image request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+function snapchat(response, postData, pathname, type){
+	console.log("snapchat Image request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+function twitter(response, postData, pathname, type){
+	console.log("twitter Image request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+
+function aboutImage(response, postData, pathname, type){
+	console.log("headerImage request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+function missionVisionImage(response, postData, pathname, type){
+	console.log("headerImage request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+function shareWarmthImage(response, postData, pathname, type){
+	console.log("headerImage request is handled");	
+	var file = "./public" + pathname;
+	fs.readFile(file, ready);
+	function ready(err, content) { deliver(response, type, err, content);}
+}
+
+ exports.index = index;
+ exports.whatwedo = whatwedo;
+ exports.blog = blog;
+ exports.whoweare = whoweare;
+ exports.news = news;
+ exports.getinvolved = getinvolved;
+ exports.wherewework = wherewework;
+ exports.cookiepolicy = cookiepolicy;
+ exports.donate = donate;
+ exports.donateform = donateform;
+ exports.contactus = contactus;
+ exports.donateportal = donateportal;
+
+ exports.login = login;
+ exports.forgetpassword = forgetpassword;
+ exports.dashboard = dashboard;
+ exports.blogform = blogform;
+ exports.newsform = newsform;
+ exports.scriptcookie = scriptcookie;
+ exports.annualreport = annualreport;
+
+ exports.authenticatefunction = authenticatefunction;
+ exports.functionblogentry = functionblogentry;
+ exports.functionwhoweare = functionwhoweare;
+
+ exports.style = style;
+ exports.scriptblogform = scriptblogform;
+ exports.scriptnewsform = scriptnewsform;
+ exports.scriptwhoweare = scriptwhoweare;
+
+ exports.scriptLogin = scriptLogin;
+
+ exports.headerImage = headerImage;
+ exports.whoWeAreHeaderImage = whoWeAreHeaderImage;
+ exports.getInvolvedHeaderImage =  getInvolvedHeaderImage;
+ exports.whereWeWorkHeaderImage = whereWeWorkHeaderImage;
+ exports.whoWeAreImage = whoWeAreImage;
+ exports.whatwedoImage = whatwedoImage;
+ exports.getInvolvedImage = getInvolvedImage;
+ exports.donateImage = donateImage;
+
+ exports.defaultprofile = defaultprofile;
+ exports.defaultpric = defaultpric;
+ // exports.newsImage2 = newsImage2;
+ // exports.newsImage3 = newsImage3;
+
+ exports.facebook = facebook;
+ exports.googleplus = googleplus;
+ exports.instagram = instagram;
+ exports.snapchat = snapchat;
+ exports.twitter = twitter;
+
+ exports.aboutImage = aboutImage;
+ exports.shareWarmthImage = shareWarmthImage;
+ exports.missionVisionImage = missionVisionImage;
+
+
+// Deliver the file that has been read in to the browser.
+function deliver(response, type, err, content) {
+    if (err) {
+    	return fail(response, NotFound, "File not found");
+    }
+    var typeHeader = { "Content-Type": type };
+    response.writeHead(OK, typeHeader);
+    response.write(content);
+    response.end();
+}
